011f8358778c64132ec662399a663a7abb195e26
SKNOBEL1 <nobelrahman444@gmail.com>
2025-04-12 22:42:23 +0600

    card part clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Blogs from './components/Blogs/Blogs'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
       <Navbar></Navbar>
+      
+      
+      <div className='main-container flex text-center'>
+        <div className='left-container w-[70%]'>
+          
+          <Blogs></Blogs>
+          
+        </div>
+        <div className='right-container w-[30%]'>
+          <h1>Reading time :0</h1>
+          <h1>bookmarked count: 0</h1>
+        </div>
+
+      </div>
+      
     </>
   )
 }
