0b045c46bed6654a8176ea9ee7c299f77eb487d9
SKNOBEL1 <nobelrahman444@gmail.com>
2025-04-13 12:19:24 +0600

    bookmark logo sate complete

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 // const Blog = (props) => {
 //     const {blog} =props;
@@ -14,8 +15,8 @@ const Blog = ({blog}) => {
     
     console.log(blog)
     return (
-        <div>
-            <h1>{blog.id}</h1>
+        <div className='m-2'>
+            {/* <h1>{blog.id}</h1> */}
             <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
     <img
@@ -23,8 +24,20 @@ const Blog = ({blog}) => {
       alt="Shoes" />
   </figure>
   <div className="card-body">
+    <div className="author flex justify-around items-center">
+      <h3>{blog.author}</h3>
+      <img className='w-16' src={blog.author_img} alt="" />
+      <FaBookmark size={25}/>
+    </div>
     <h2 className="card-title">{blog.title}</h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    
+     <div className="flex">
+     {
+      blog.hashtags.map((has)=> <p>{has}</p>)
+    }
+     </div>
+
     <div className="card-actions justify-end">
       <button className="btn btn-primary">mark as read</button>
     </div>
